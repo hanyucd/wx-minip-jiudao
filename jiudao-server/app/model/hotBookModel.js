94e3af6c0c721a2724d526c9f786fbef735a2b52
hanyucd <1807003141@qq.com>
2021-09-05 18:48:04 +0800

    style: 优化

--- jiudao-server/app/model/hotBookModel.js
+++ jiudao-server/app/model/hotBookModel.js
@@ -24,8 +24,23 @@ class HotBook extends Model {
       group: 'artId', // group对art_id进行分组，但如果对多个数组分组的话是一个笛卡尔积
       attributes: [ 'artId', [Sequelize.fn('COUNT', '*'), 'count'] ]
     });
-    
-    return { ids, favors };
+
+    books.forEach(book => {
+      HotBook._getEachBookStatus(book, favors);
+    });
+
+    return { books, ids, favors };
+  }
+
+  static _getEachBookStatus(book, favors) {
+    let count = 0;
+    favors.forEach(favor => {
+      if (book.id === favor.artId) {
+        count = favor.get('count');
+      }
+    });
+    book.setDataValue('favNums', count);
+    return book;
   }
 }
 
